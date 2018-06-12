f95ad6a2cca4e07a700ecdaa7afc3b1d5969565e
Ninh Nguyen <nguyenninh.uit@gmail.com>
2018-06-12 11:10:14 +0700

    Update keyboard

--- NinhTweeter/NinhTweeter/ViewController.swift
+++ NinhTweeter/NinhTweeter/ViewController.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var tbTweet: UITableView!
     @IBOutlet weak var tfTweet: UITextField!
-    @IBOutlet weak var btnSend: UIButton!
+    
+    @IBOutlet weak var lcVTextBottom: NSLayoutConstraint!
     
     var listTweets = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.setUpForKeyBoard()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,14 +28,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Button Action
+    deinit {
+        self.releaseForKeyBoard()
+    }
     
-    @IBAction func btnSendAction(_ sender: Any) {
-        
+    // MARK: - Keyboard return
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("TextField return")
+        return false
     }
     
     // MARK: - Keyboard
     
+    func setUpForKeyBoard() {
+        // register key board
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShowNotification(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        //
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHideNotification(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func releaseForKeyBoard() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShowNotification(notification: NSNotification) {
+        //print("keyboard displaing!!")
+        //
+        let info:NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardFrame:NSValue = info.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue
+        var kbRect:CGRect = keyboardFrame.cgRectValue
+        
+        kbRect = self.view.convert(kbRect, to: nil)
+        let kbSize = kbRect.size
+        
+        let duration:NSNumber = info.object(forKey: UIKeyboardAnimationDurationUserInfoKey) as! NSNumber
+        
+        self.lcVTextBottom.constant = kbSize.height
+        UIView.animate(withDuration: duration.doubleValue) { [weak self] in
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHideNotification(notification: NSNotification) {
+        //print("keyboard hidding!!")
+        //
+        let info:NSDictionary = notification.userInfo! as NSDictionary
+        let duration:NSNumber = info.object(forKey: UIKeyboardAnimationDurationUserInfoKey) as! NSNumber
+        
+        self.lcVTextBottom.constant = 0.0
+        UIView.animate(withDuration: duration.doubleValue) { [weak self] in
+            self?.view.layoutIfNeeded()
+        }
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
