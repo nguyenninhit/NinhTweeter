ec47c5dc0a1a445656cc08ee2c0ba14da4c36b67
Ninh Nguyen <nguyenninh.uit@gmail.com>
2018-06-12 10:55:12 +0700

    Update UI

--- NinhTweeter/NinhTweeter/ViewController.swift
+++ NinhTweeter/NinhTweeter/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tbTweet: UITableView!
+    @IBOutlet weak var tfTweet: UITextField!
+    @IBOutlet weak var btnSend: UIButton!
+    
+    var listTweets = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - Button Action
+    
+    @IBAction func btnSendAction(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Keyboard
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listTweets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        var cell : UITableViewCell!
+        cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        //
+        cell.textLabel?.text = listTweets[indexPath.row]
+        //
+        return cell
+    }
+}
